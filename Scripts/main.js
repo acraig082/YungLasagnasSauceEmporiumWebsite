62a4ad1b7ecd633eff87d0256afb2b4c44545f45
acraig082 <acraig082@gmail.com>
2018-01-02 14:53:27 -0600

    ass

--- Scripts/main.js
+++ Scripts/main.js
@@ -29,12 +29,12 @@ function setUserName() {
   var myName = prompt('Please enter your new skin.');
   localStorage.setItem('name', myName);
   myHeading.textContent = 'come get that cheese ' + myName;
-  if(localStorage.getItem('name')==='pizza'||'Pizza'){
+  if(localStorage.getItem('name')==='pizza'){
     window.alert('***THE FBI HAS BEEN NOTIFIED***');
     window.alert('***THE FBI HAS BEEN NOTIFIED***');
     window.alert('***THE FBI HAS BEEN NOTIFIED***');
-  }
-  if(localStorage.getItem('name')==='nolan'||'Nolan'){
+  }else
+  if(localStorage.getItem('name')==='nolan' || localStorage.getItem('name')==='Nolan'){
     window.alert('thats the gayest thing ive ever heard');
   }
 }
